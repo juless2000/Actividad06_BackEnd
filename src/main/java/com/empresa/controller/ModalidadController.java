790ef774fe1eff7115adf6d56b3c31ca72a614d7
juless2000 <munozjulian16@gmail.com>
2021-10-14 13:52:50 -0500

    actividad06

--- src/main/java/com/empresa/controller/ModalidadController.java
+++ src/main/java/com/empresa/controller/ModalidadController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.empresa.entity.FiltroModalidad;
 import com.empresa.entity.Modalidad;
 import com.empresa.service.ModalidadService;
 import com.empresa.util.Constantes;
@@ -50,5 +52,29 @@ public class ModalidadController {
 		}
 		return ResponseEntity.ok(salida);
 	}
+	
+	@GetMapping("/porFiltroModalidadConJson")
+	@ResponseBody
+	public ResponseEntity<Map<String, Object>> listaModalidadConParametros(
+							@RequestBody FiltroModalidad filtro) {
+		
+		Map<String, Object> salida = new HashMap<String, Object>();
+		try {
+			filtro.setNombre("%"+filtro.getNombre()+"%");
+			filtro.setSede("%"+filtro.getSede()+"%");
+			List<Modalidad> lista = modalidadService.listaPorFiltro(filtro);
+			if(CollectionUtils.isEmpty(lista)){
+				salida.put("mensaje", "No existe elementos para la consulta");
+			}else {
+				salida.put("lista", lista);
+				salida.put("mensaje", "Se tiene " + lista.size() + " elementos");
+			}
+		} catch (Exception e) {
+			salida.put("mensaje", "Error : " + e.getMessage());
+		}
+		
+		return ResponseEntity.ok(salida);
+	}
+	
 
 }
